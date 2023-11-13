fdacb3e15c131d0ab832d459feec49b916bb6ea8
Bruno Castillo Aviles <brunocc.0205@gmail.com>
2023-11-13 20:23:22 -0300

    Modelos creados

--- Proyecto3va/Dante/Inventario/models.py
+++ Proyecto3va/Dante/Inventario/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Suppliers(models.Model):
+    name=models.CharField(max_length=100)
+    run=models.CharField(max_length=10)
+    cellphone=models.PositiveIntegerField()
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name=models.CharField(max_length=100,verbose_name=("Name"))
+    name_category=models.ForeignKey("Inventario.Category", verbose_name=("Name Category"), on_delete=models.PROTECT)
+    price_sold=models.PositiveIntegerField(verbose_name=("Price Sold"))
+    buy_price=models.PositiveIntegerField(verbose_name=("Buy Price"))
+    stock=models.PositiveIntegerField(verbose_name=("Stock"))
+    bar_code=models.IntegerField(verbose_name=("Bar Code"))
+    minimum_amount=models.PositiveIntegerField(verbose_name=("Minimum Amount"))
+    suppliers=models.ForeignKey("Inventario.Suppliers", verbose_name=("Suppliers"), on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
--- Proyecto3va/Dante/Venta/models.py
+++ Proyecto3va/Dante/Venta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Venta(models.Model):
+    fecha = models.DateTimeField(auto_now_add=True)
+    
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey('Inventario.Product', on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField(default=1)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+   
