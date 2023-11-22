d0052b86dfa44aa8d80a572930e1989ee0131e13
Bruno Castillo Aviles <brunocc.0205@gmail.com>
2023-11-22 01:53:31 -0300

    CRUD completo con redirect bueno con alerts

--- Proyecto3va/Dante/Dante/urls.py
+++ Proyecto3va/Dante/Dante/urls.py
@@ -21,13 +21,27 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    #INVENTARIO
+    #VISTAS GENERALES
     path('admin/', admin.site.urls),
     path('', inventario.home, name="home"),
     path('categorias/', inventario.categorias, name="categorias"),
+    path('proveedores/',inventario.listaProveedores, name='proveedores'),
+    path('categorias/list/<str:nombre>/', inventario.listaProductos, name="listaProductos"),
+    #Creacion
     path('categorias/add/', inventario.crear_categoria, name="crearCategorias"),
-    path('categorias/list//<str:nombre>/', inventario.listaProductos, name="listaProductos"),
     path('productos/Add/',inventario.crear_producto,name='crearProducto'),
     path('proveedor/Add/',inventario.crear_proveedor,name='crearProveedor'),
+    #Modificar
+    path('productos/edit/<int:producto_id>',inventario.editar_producto,name='editarProducto'),
+    path('categorias/edit/<int:categoria_id>',inventario.editar_categoria,name='editarCategoria'),
+    path('proveedor/edit/<int:proveedor_id>',inventario.editar_proveedor,name='editarProveedor'),
+    #Eliminar
+    path('productos/eliminado/<int:id>/',inventario.eliminar_producto,name='eliminarProducto'),
+    path('categorias/eliminado/<int:id>/',inventario.eliminar_categoria,name='eliminarCategoria'),
+    path('proveedor/eliminado/<int:id>/',inventario.eliminar_proveedor,name='elimminarProveedor'),
+
+    #VENTA
     
 ]
 if settings.DEBUG:
--- Proyecto3va/Dante/Inventario/views.py
+++ Proyecto3va/Dante/Inventario/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from Inventario.forms import *
 from Inventario.models import *
+from django.contrib import messages
 
 
 # Create your views here.
+#VISTAS GENERALES PAGINAS PRINCIPALES
 def home(request):
     return render(request,"base.html")
 
@@ -11,20 +13,30 @@ def categorias(request):
     categorias=Category.objects.all()
     return render(request,'inventario/Categorias.html',{'categorias':categorias})
 
+def listaProductos(request, nombre):
+    categoria = Category.objects.get(name=nombre)
+    productos = Product.objects.filter(name_category=categoria)
+    return render(request, 'inventario/listaProductos.html', {'categorias': categoria, 'productos': productos})
+
+def listaProveedores(request):
+    proveedores = Suppliers.objects.all()
+   
+    return render(request, 'inventario/proveedores.html',{'proveedores': proveedores})
+
+
+
+#METODOS CREATES
+
 def crear_categoria(request):
     if request.method=="POST":
         form=CategoryForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request,"Creado Correctamente")
             return redirect('categorias')
     else:
         form=CategoryForm()
     return render(request,'inventario/CategoriasAdd.html',{'form':form})
-
-def listaProductos(request, nombre):
-    categoria = Category.objects.get(name=nombre)
-    productos = Product.objects.filter(name_category=categoria)
-    return render(request, 'inventario/listaProductos.html', {'categorias': categoria, 'productos': productos})
 
 # def crear_producto(request):
 #     if request.method=="POST":
@@ -38,31 +50,114 @@ def listaProductos(request, nombre):
 def crear_producto(request):
     if request.method == "POST":
         form_producto = ProductForm(request.POST, request.FILES)
-        form_categoria = CategoryForm(request.POST, request.FILES)
         if form_producto.is_valid():
             producto = form_producto.save(commit=False)
-            # Aquí puedes realizar operaciones adicionales con el objeto del producto si es necesario
+            
+            # Guarda el producto y obtén la categoría asociada
             producto.save()
 
-            # Si también necesitas guardar la categoría, puedes hacer lo mismo con el formulario de categoría
-            if form_categoria.is_valid():
-                categoria = form_categoria.save(commit=False)
-                # Realiza las operaciones adicionales con el objeto de categoría si es necesario
-                categoria.save()
-
-            return redirect('home')
+            # Asumo que el campo que almacena la categoría se llama 'name_category'
+            nombre_categoria = producto.name_category.name
+            
+            # Redirige a la página de la lista de productos de la categoría asociada
+            return redirect('listaProductos', nombre=nombre_categoria)
     else:
         form_producto = ProductForm()
-        form_categoria = CategoryForm()
 
-    return render(request, 'inventario/productosAdd.html', {'form_producto': form_producto, 'form_categoria': form_categoria})
+    return render(request, 'inventario/productosAdd.html', {'form_producto': form_producto})
 
 def crear_proveedor(request):
     if request.method=="POST":
         form=SuppliersForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            messages.success(request,"Creado Correctamente")
+            return redirect('proveedores')
     else:
         form=SuppliersForm()
     return render(request,'inventario/proveedorAdd.html',{'form':form})
+
+
+#METODOS EDITAR
+
+def editar_categoria(request,categoria_id):
+     categoria = get_object_or_404(Category, id=categoria_id)
+    
+     if request.method == "POST":
+        form = CategoryForm(request.POST, request.FILES, instance=categoria)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Modificado Correctamente")
+            return redirect('categorias')
+     else:
+        form = CategoryForm(instance=categoria)
+    
+     return render(request, 'inventario/mantenedorCategoria.html', {'form': form})
+
+def editar_producto(request,producto_id):
+     producto = get_object_or_404(Product, id=producto_id)
+    
+     if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=producto)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Modificado Correctamente")
+
+            return redirect('listaProductos', nombre=producto.name_category.name)
+     else:
+        form = ProductForm(instance=producto)
+    
+     return render(request, 'inventario/mantenedorProducto.html', {'form': form})
+
+def editar_proveedor(request,proveedor_id):
+     proveedor = get_object_or_404(Suppliers, id=proveedor_id)
+    
+     if request.method == "POST":
+        form = SuppliersForm(request.POST, request.FILES, instance=proveedor)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Modificado Correctamente")
+            return redirect('proveedores')
+     else:
+        form = SuppliersForm(instance=proveedor)
+    
+     return render(request, 'inventario/mantenedorProveedor.html', {'form': form})
+
+
+#METODOS ELIMINAR
+
+def eliminar_producto(request, id):
+    producto = get_object_or_404(Product, id=id)
+
+    # Obtener la imagen asociada al producto (si existe)
+    
+    producto.delete()
+    messages.success(request,"Eliminado correctamente")
+    # Obtener la URL referer (página anterior)
+    redirect_url = request.META.get('HTTP_REFERER', '/')
+
+    return redirect(redirect_url)
+
+def eliminar_categoria(request, id):
+    categoria = get_object_or_404(Category, id=id)
+
+    # Obtener la imagen asociada al producto (si existe)
+    
+    categoria.delete()
+    messages.success(request,"Eliminado correctamente")
+    # Obtener la URL referer (página anterior)
+    
+
+    return redirect('categorias')
+
+def eliminar_proveedor(request, id):
+    proveedor = get_object_or_404(Suppliers, id=id)
+
+    # Obtener la imagen asociada al producto (si existe)
+    
+    proveedor.delete()
+    messages.success(request,"Eliminado correctamente")
+    # Obtener la URL referer (página anterior)
+    
+
+    return redirect('proveedores')
